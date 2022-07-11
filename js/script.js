ab4ed0b470e6b2698105669e4e2b839fed9b7035
torone89 <torone89>
2022-07-11 13:22:33 +0200

    Esercizio Base

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ console.log("JS")
 // VERIFICA COLLEGAMENTO VUEJS
 
 console.log('Vue ok', Vue)
+
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const root = new Vue({
+    el: '#root',
+    name: 'Start Vue',
+    data: {
+        saluto: "Ciao sono Francesco"
+    }
+})
